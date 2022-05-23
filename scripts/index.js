34f64534ce021ae1fef5355cc93928df1686f585
Andrey Zhukov <settler59@yandex.ru>
2022-05-23 12:47:18 +0300

    четвертый спринт правки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,36 @@ let profileEdit = document.querySelector ('.profile__edit-button')
 let popup = document.querySelector ('.popup') 
 let profileName =  document.querySelector ('.profile__title')
 let profileJob = document.querySelector ('.profile__subtitle')
-let popupName = document.querySelector ('.popup__input-title')
-let popupJob = document.querySelector ('.popup__input-subtitle')
+let popupName = document.querySelector ('.popup__input_type_title')
+let popupJob = document.querySelector ('.popup__input_type_subtitle')
 let formElement = document.querySelector ('.popup__form')
 let profileEditExit = document.querySelector ('.popup__close-button')
 
-profileEdit.addEventListener('click', function () {
-  popup.classList.add('popup_type_opened');
+function popupOpen(popupName) {
+  popupName.classList.add('popup_opened');
+}
+
+profileEdit.addEventListener('click', function() {
+  popupOpen(popup);
   popupName.value = profileName.textContent;
   popupJob.value = profileJob.textContent;
 })
 
+function popupClose (popupName) {
+  popupName.classList.remove('popup_opened');
+}
+
 profileEditExit.addEventListener('click', function () {
-  popup.classList.remove('popup_type_opened');
+  popupClose(popup);
 })
+
+function poopupSubmit(popupName) {
+  popupName.classList.remove('popup_opened');
+} 
 
 formElement.addEventListener ('submit', function(event){
   event.preventDefault();
+  poopupSubmit(popup);
   profileName.textContent = popupName.value;
-  profileJob.textContent = popupJob.value;
-  popup.classList.remove('popup_type_opened');
+  profileJob.textContent = popupJob.value
 }) 
